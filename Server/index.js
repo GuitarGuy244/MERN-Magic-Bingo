a75b9bf5eda1524daa9d8c964818136fbbbaeaae
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-10-17 14:47:09 -0700

    247

--- Server/index.js
+++ Server/index.js
@@ -12,10 +12,13 @@ const cors = require("cors");
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
-mongoose.connect("mongodb://localhost:auth/bingo", { useNewUrlParser: true });
+mongoose.connect("mongodb://master:aurora726@ds335648.mlab.com:35648/heroku_xcqt8p7q", { useNewUrlParser: true });
 
 //app setup
-// app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
 app.use(morgan("combined"));
 app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));

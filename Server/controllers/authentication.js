197c47d4ceae31ca7de3954a57413ce85ff61b07
GuitarGuy244 <rgsl.salazar@gmail.com>
2019-10-16 18:58:18 -0700

    Update authentication.js

--- Server/controllers/authentication.js
+++ Server/controllers/authentication.js
@@ -53,5 +53,5 @@ exports.register = function (req, res, next) {
 
 exports.addReading = function (req, res, next) {
   const reading = req.body.reading;
-  User.findOneAndUpdate({ email: req.user.email }, { $push: { reading: reading } });
+  User.findOneAndUpdate({ email: req.body.email }, { $push: { reading: reading } });
 }
